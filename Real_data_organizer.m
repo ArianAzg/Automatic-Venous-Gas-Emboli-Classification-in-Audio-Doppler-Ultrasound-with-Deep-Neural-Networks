d9b72d0ebea25f0962149b3fa9f519860da409cf
Arian Azarang <48659018+ArianAzg@users.noreply.github.com>
2022-07-02 15:41:59 -0400

    Update Real_data_organizer.m

--- Real_data_organizer.m
+++ Real_data_organizer.m
@@ -7,10 +7,11 @@ close all
 
 %% Reference
 
-%       D.Q. Le*, A. Azarang*, A. Hoang, L. Blogg, P.A. Dayton, R. Lance, M. Natoli, A. Gatrell, 
-%       R.E. Moon, P. Lindholm, and V. Papadopoulou, "Deep Learning-Based Venous Gas Emboli Grade 
+%       A. Azarang*, D.Q. Le*, A. Hoang, L. Blogg, P.A. Dayton, R. Lance, M. Natoli, A. Gatrell, 
+%       Frauke Tillmans, R.E. Moon, P. Lindholm, and V. Papadopoulou, "Deep Learning-Based Venous Gas Emboli Grade 
 %       Classification in Doppler Ultrasound Audio Recordings"
-
+        
+%       *Authors contributed equally
 %% ************Tip************
 
 % The value "Path_to_Real_data_folder" must correctly set for show the
@@ -42,7 +43,7 @@ for i = 1:test_len
     %         Subject4:
     %         grade4: The Spencer grade of this selected subject
     %         %d: Will iterate to count down number of available 5-s data           
-    data = s(i*40000 - 39999: i*40000);
+    data = s(i*80000 - 79999: i*80000);
     audiowrite(strcat('C:\Users\azarang\Desktop\Postdoc\deep_learning_audio_classification\Real_data\Subclavian_real_data_5_s_clips_11-10-2021\4', '\', filename), data/max(abs(data)), fs_t);
 end
 
